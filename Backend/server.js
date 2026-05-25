6a8f796223ef4229cd1006b90e4f37e3c1d13af0
divya-sigma <divyasreeviswanadh@gmail.com>
2026-05-25 13:38:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", chatRoutes);
